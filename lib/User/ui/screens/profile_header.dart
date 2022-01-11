31d8a0162f181aaeebe3c6b22a53bff7493ca372
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-01-11 14:32:31 -0500

    Mostrando los datos de usuario de Google en la interfaz en Flutter

--- lib/User/ui/screens/profile_header.dart
+++ lib/User/ui/screens/profile_header.dart
@@ -1,39 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_avanzado/User/bloc/bloc_user.dart';
+import 'package:platzi_trips_avanzado/User/model/user.dart';
 import 'package:platzi_trips_avanzado/User/ui/widgets/button_bar.dart';
 import 'package:platzi_trips_avanzado/User/ui/widgets/user_info.dart';
 
 class ProfileHeader extends StatelessWidget {
+  late UserBloc userBloc;
+  late User user;
+
   @override
   Widget build(BuildContext context) {
-
-    final title = Text(
-      'Profile',
-      style: const TextStyle(
-          fontFamily: 'Lato',
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 30.0
-      ),
-    );
-
-    return Container(
-      margin: const EdgeInsets.only(
-          left: 20.0,
-          right: 20.0,
-          top: 50.0
-      ),
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              title
-            ],
-          ),
-          UserInfo('assets/img/ann.jpg', 'Anahí Salgado','anahi@platzi.com'),
-          ButtonsBar()
-        ],
-      ),
-    );
+    userBloc = BlocProvider.of(context);
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return CircularProgressIndicator();
+            case ConnectionState.active:
+            case ConnectionState.done:
+              return showProfileData(snapshot);
+          }
+        },
+        stream: userBloc.streamFirebase);
   }
 
+  Widget showProfileData(AsyncSnapshot snapshot) {
+    if (!snapshot.hasData || snapshot.hasError) {
+      return Container(
+        margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: const [
+                CircularProgressIndicator(),
+                Text("No se pudo cargar la informacion")
+              ],
+            ),
+          ],
+        ),
+      );
+    } else {
+      print(snapshot);
+      user  = User(name: snapshot.data.displayName, email: snapshot.data.email, photoUrl: snapshot.data.photoURL);
+      const title = const Text(
+        'Profile',
+        style: TextStyle(
+            fontFamily: 'Lato',
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 30.0),
+      );
+
+      return Container(
+        margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: [title],
+            ),
+            UserInfo(user),
+            ButtonsBar()
+          ],
+        ),
+      );
+    }
+  }
 }
