95f9b55093da764558b5fa3632864281810a1a34
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-01-11 09:46:33 -0500

    Autenticación de Firebase con Google

---
+++ lib/User/repository/firebase_auth_api.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FireBaseAuthAPI {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  Future<User> signIn() async {
+    GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+    GoogleSignInAuthentication? gSa = await googleSignInAccount?.authentication;
+
+    User user = (await _auth.signInWithCredential(
+      GoogleAuthProvider.credential(idToken:gSa?.idToken , accessToken: gSa?.accessToken )
+    )) as User;
+
+    return user;
+  }
+}
