84b2d5feb759cb8544f69db246190b8fa9c0b937
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-01-11 11:55:55 -0500

    Manejando una sesión con Firebase Authentication y Flutter

--- lib/User/bloc/bloc_user.dart
+++ lib/User/bloc/bloc_user.dart
@@ -1,10 +1,18 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:platzi_trips_avanzado/User/repository/auth_repository.dart';
 
 class UserBloc implements Bloc{
 
   final _auth_repository = AuthRepository();
+  //Flujo de datos - Streams
+  //Stream de Firebase
+  // Si no se tuviera ese stream que viene en Firebase, utilizariamos StreamController
+  Stream<User?> streamFirebase = FirebaseAuth.instance.authStateChanges();
+  Stream<User?> get authStatus => streamFirebase;
+
+
   //CASOS DE USO
 
   Future<User?> signIn(){
--- lib/User/ui/screens/signin_screen.dart
+++ lib/User/ui/screens/signin_screen.dart
@@ -1,51 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:platzi_trips_avanzado/User/bloc/bloc_user.dart';
+import 'package:platzi_trips_avanzado/platzi_trips.dart';
 import 'package:platzi_trips_avanzado/widgets/button_green.dart';
 import 'package:platzi_trips_avanzado/widgets/gradient_back.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
-class SignInScreen extends StatefulWidget{
+class SignInScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _SignInScreen();
   }
 }
 
-class _SignInScreen extends State<SignInScreen>{
+class _SignInScreen extends State<SignInScreen> {
   late UserBloc userBloc;
 
   @override
   Widget build(BuildContext context) {
     userBloc = BlocProvider.of(context);
-    return signInGoogleUi();
+    return _handleCurrentSession();
   }
 
-  Widget signInGoogleUi(){
+  Widget _handleCurrentSession() {
+    return StreamBuilder(
+      stream: userBloc.authStatus,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if(!snapshot.hasData || snapshot.hasError){
+          return signInGoogleUi();
+        }else{
+         return PlatziTrips();
+        }
+      },
+    );
+  }
+
+  Widget signInGoogleUi() {
     return Scaffold(
       body: Stack(
         alignment: Alignment.center,
         children: [
-          GradientBack("",MediaQuery.of(context).size.height),
+          GradientBack("", MediaQuery.of(context).size.height),
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children:  [
-              const Text("Welcome \n This is your Travel App",style:
-                TextStyle(
-                  fontSize: 37.0,
-                  fontFamily: "Lato",
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
-                )),
-              ButtonGreen(text: "Login with Gmail", onPressed: (){
-                userBloc.signIn().then((value) => {
-                  print(value)
-                });
-              }, heigth: 50, width: 300)
+            children: [
+              const Text("Welcome \n This is your Travel App",
+                  style: TextStyle(
+                      fontSize: 37.0,
+                      fontFamily: "Lato",
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold)),
+              ButtonGreen(
+                  text: "Login with Gmail",
+                  onPressed: () {
+                    userBloc.signIn().then((value) => {print(value?.displayName)});
+                  },
+                  heigth: 50,
+                  width: 300)
             ],
           )
         ],
       ),
     );
   }
-
 }
