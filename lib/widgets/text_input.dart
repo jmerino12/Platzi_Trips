ec6945303c615cc0378788d44b42894ca4260bbd
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-01-12 11:21:04 -0500

    Widget TextField personalizado en Flutter

---
+++ lib/widgets/text_input.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class TextInput extends StatelessWidget {
+  final String hintText;
+  final TextInputType inputType;
+  final TextEditingController controller;
+  int? maxLines = 1;
+
+  TextInput(
+      {Key? key,
+      required this.hintText,
+      required this.inputType,
+      required this.controller,
+      this.maxLines});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.only(right: 20, left: 20),
+        child: TextField(
+            controller: controller,
+            keyboardType: inputType,
+            style: const TextStyle(
+                fontSize: 15,
+                fontFamily: "Lato",
+                color: Colors.blueGrey,
+                fontWeight: FontWeight.bold),
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: Color(0xFFE5E5E5),
+                border: InputBorder.none,
+                hintText: hintText,
+                enabledBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(9)),
+                  borderSide: BorderSide(color: Color(0xFFE5E5E5)),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(9)),
+                  borderSide: BorderSide(color: Color(0xFFE5E5E5)),
+                ))));
+  }
+}
